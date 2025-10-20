40fbd7d018a180af5bf23419458bdad77201d8f6
Marcos H. Hisano Oshiro <marcos.2h.oshiro@gmail.com>
2025-10-20 10:52:31 -0400

    getting json body

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
 import express from "express"
 
 const app = express()
+app.use(express.json())
 
 app.get("/product/:id/:user", (request, response) => {
     const { id, user } = request.params
 
     response.send("Hello world")
 })
+
+app.post("/product", (request, response) => {
+    const { name, price } = request.body
+
+    response.send("Hello world")
+})
+
 
 app.listen(3333, () => console.log("Server is running"))
