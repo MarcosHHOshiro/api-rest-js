a7cab5ccdf53152cb1245e9d476cf01af225be61
Marcos H. Hisano Oshiro <marcos.2h.oshiro@gmail.com>
2025-10-20 12:46:37 -0400

    Adding global middlewares

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from "express"
+import { myMiddleware } from "./middlewares/my-middlewares"
 
 const app = express()
 app.use(express.json())
+
+app.use(myMiddleware)
 
 app.get("/product/:id/:user", (request, response) => {
     const { id, user } = request.params
@@ -12,7 +15,7 @@ app.get("/product/:id/:user", (request, response) => {
 app.post("/product", (request, response) => {
     const { name, price } = request.body
 
-    response.send("Hello world")
+    response.json({ name, price })
 })
 
 
